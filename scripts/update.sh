b822bb0a699b51ea6527793d097b99a933080c5e
Raiden Kaneda <raiden@Raidens-MacBook-Pro.local>
2017-08-16 16:00:04 -0700

    updated public files

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,13 +2,13 @@
 LOC=https://github.com/RKaneda9/ninjafit1.0.git
 
 echo "Retrieving ninjafit1.0 from ${LOC} into temp folder..."
-git clone ${LOC} scripts/temp
+git clone ${LOC} ./scripts/temp
 
 echo "Moving built folders and files into public folder..."
-cp -R -v ./scripts/temp/public/**/* ./public
+cp -R -v ./scripts/temp/public/* ./public
 
 echo "Removing temp folder..."
-rm -rf scripts/temp
+rm -rf ./scripts/temp
 
 echo "Updating ninjafit1.0.api..."
 git pull origin master
